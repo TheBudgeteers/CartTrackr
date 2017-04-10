fdcdafd4a2481d2b94a5d6fc1b5980b73e0ac0e1
cleex1004 <cleex1004@gmail.com>
2017-04-10 09:26:02 -0700

    initial setup

---
+++ CartTrackr/CartTrackr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CartTrackr
+//
+//  Created by Christina Lee on 4/10/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
