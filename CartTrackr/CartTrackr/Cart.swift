b89ca57b4be40a98c2f654a1797df04fc05c0ec6
Brandon Little <brlittle86@gmail.com>
2017-04-10 10:47:33 -0700

    Created Cart.swift and added basic code to make it a true singleton

---
+++ CartTrackr/CartTrackr/Cart.swift
@@ -0,0 +1,19 @@
+//
+//  Cart.swift
+//  CartTrackr
+//
+//  Created by Brandon Little on 4/10/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+import Foundation
+
+class Cart {
+    private var listItems = [Item]()
+    
+    static let shared = Cart()
+    
+    private init() {
+        
+    }
+}
