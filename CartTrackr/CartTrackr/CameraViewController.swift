01859a2778ee05c2f825aa5c4b2e04a3834d7579
Rio Balderas <Rio@Apples-MacBook-Air.local>
2017-04-10 17:38:51 -0700

    imported SwiftyCam framework and updated some code in CameraViewController, Two errors due to classes not built yet.

--- CartTrackr/CartTrackr/CameraViewController.swift
+++ CartTrackr/CartTrackr/CameraViewController.swift
@@ -8,26 +8,77 @@
 
 import UIKit
 import AVFoundation
+import SwiftyCam
 
-class CameraViewController: UIViewController {
+
+class CameraViewController: SwiftyCamViewController {
 
     //OUTLET for "previewView" goes here for UIView.
     //OUTLET for "captureImageView" goes here for UIImageView.
     //ACTION "didTakePhoto" goes here for take picture Button.
     
-    var session: AVCaptureSession?
-    var itemImageOutput: AVCapturePhotoOutput?
-    var previewLayer: AVCaptureVideoPreviewLayer?
-    
+    var flipCameraButton: UIButton!
+    var flashButton: UIButton!
+
     //xcode linter made me write init like this below, AVFoundation thing???//////
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) is needed for NSCoder")
+        fatalError("init(coder:) has not been implemented")
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let captureButton = SwiftyCamButton(frame: "name of outlet goes here")
+        captureButton.delegate = self
+        
+        cameraDelegate = self
+        shouldUseDeviceOrientation = true
+        
+        addButtons()
 
     }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didTake photo: UIImage) {
+        let newVC = PhotoViewController(image: photo)
+        self.present(newVC, animated: true, completion: nil)
+    }
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFocusAtPoint point: CGPoint) {
+        let focusView = UIImageView(image: #imageLiteral(resourceName: "focus"))
+        focusView.center = point
+        focusView.alpha = 0.0
+        view.addSubview(focusView)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveEaseInOut, animations: {
+            focusView.alpha = 1.0
+            focusView.transform = CGAffineTransform(scaleX: 1.25, y: 1.25)
+        }, completion: { (success) in
+            UIView.animate(withDuration: 0.15, delay: 0.5, options: .curveEaseInOut, animations: {
+                focusView.alpha = 0.0
+                focusView.transform = CGAffineTransform(translationX: 0.6, y: 0.6)
+            }, completion: { (success) in
+                focusView.removeFromSuperview()
+            })
+        })
+    }
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didChangeZoomLevel zoom: CGFloat) {
+        print(zoom)
+    }
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didSwitchCameras camera: SwiftyCamViewController.CameraSelection) {
+        print(camera)
+    }
+
+    
     //Checker used to check authorization of using camera
     func checkCameraAuthorization(_ completionHandler: @escaping((_ authorized: Bool)-> Void)){
         switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo){
@@ -45,54 +96,28 @@ class CameraViewController: UIViewController {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        //Will only proceed if camera is authorizd through above function
-        self.checkCameraAuthorization { authorized in
-            if authorized {
-                
-                //prepares image for parsing(under 10mb parse max) normally "AVCaptureSessionPresetPhoto"
-                self.session!.sessionPreset = AVCaptureSessionPresetMedium
-                
-                let rearCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-                
-                var error: NSError?
-                var input: AVCaptureDeviceInput!
-                
-                do {
-                    input = try AVCaptureDeviceInput(device: rearCamera)
-                } catch let error1 as NSError {
-                    error = error1
-                    input = nil
-                    print(error!.localizedDescription)
-                }
-                
-                if error == nil && self.session!.canAddInput(input) {
-                    //connects input for camera use
-                    self.session!.addInput(input)
-                    self.itemImageOutput = AVCapturePhotoOutput()
-                    func capturePhoto() {
-                        let previewPixelType = AVCapturePhotoSettings().availablePreviewPhotoPixelFormatTypes.first!
-                        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
-                                             kCVPixelBufferWidthKey as String: 160,
-                                             kCVPixelBufferHeightKey as String: 160]
-                        AVCapturePhotoSettings().previewPhotoFormat = previewFormat
-                        
-                        self.itemImageOutput?.capturePhoto(with: AVCapturePhotoSettings(), delegate: self as! AVCapturePhotoCaptureDelegate)
-                        
-                    }
-                   
-                }
-            } else {
-                print("Permission to use camera denied.")
-            }
-        }
+    private func addButtons() {
         
         
+        flipCameraButton = UIButton(frame: CGRect(x: (((view.frame.width / 2 - 37.5) / 2) - 15.0), y: view.frame.height - 74.0, width: 30.0, height: 23.0))
+        flipCameraButton.setImage(#imageLiteral(resourceName: "flipCamera"), for: UIControlState())
+        flipCameraButton.addTarget(self, action: #selector(cameraSwitchAction(_:)), for: .touchUpInside)
+        self.view.addSubview(flipCameraButton)
+        
+        let test = CGFloat((view.frame.width - (view.frame.width / 2 + 37.5)) + ((view.frame.width / 2) - 37.5) - 9.0)
+        
+        flashButton = UIButton(frame: CGRect(x: test, y: view.frame.height - 77.5, width: 18.0, height: 30.0))
+        flashButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControlState())
+        flashButton.addTarget(self, action: #selector(toggleFlashAction(_:)), for: .touchUpInside)
+        self.view.addSubview(flashButton)
     }
     
     
 
+
+}
+
+extension CameraViewController: SwiftyCamViewControllerDelegate{
 
 }
 
