a020a1d2b01caef6ed59702f5d048610fe0a83c4
Rio <jayb967@gmail.com>
2017-06-13 14:09:01 -0600

    automatic camera recognition

--- CartTrackr/CartTrackr/AsynchronousCameraReading.swift
+++ CartTrackr/CartTrackr/AsynchronousCameraReading.swift
@@ -8,36 +8,133 @@
 
 import UIKit
 import AVFoundation
-import SwiftyCam
 
 protocol FrameDelegate: class {
     func captured(image: UIImage)
 }
 
-class AsynchronousCameraReading:  NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
-//    var cameraView: CameraViewController! = nil
+class AsynchronousCameraReading: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-
-    private let sessionQueue = DispatchQueue(label: "session queue")
-    private let position = AVCaptureDevicePosition.back
-    private let quality = AVCaptureSessionPresetMedium
+    
+    private var position = AVCaptureDevicePosition.back
+    private let quality = AVCaptureSessionPresetHigh
+    
     private var permissionGranted = false
+    private let sessionQueue = DispatchQueue(label: "session queue")
     private let captureSession = AVCaptureSession()
     private let context = CIContext()
+    var flash:Bool! = false
     
+    weak var delegate: FrameDelegate?
     
-     //required init is needed when using AVFoundation
-    override init(){
-        super .init()
-        
-        self.permissionGranted = true
-        sessionQueue.async {
+    override init() {
+        super.init()
+        checkPermission()
+        sessionQueue.async { [unowned self] in
             self.configureSession()
             self.captureSession.startRunning()
         }
     }
-     weak var delegate: FrameDelegate?
     
+    public func flipCamera() {
+        sessionQueue.async { [unowned self] in
+            self.captureSession.beginConfiguration()
+            guard let currentCaptureInput = self.captureSession.inputs.first as? AVCaptureInput else { return }
+            self.captureSession.removeInput(currentCaptureInput)
+            guard let currentCaptureOutput = self.captureSession.outputs.first as? AVCaptureOutput else { return }
+            self.captureSession.removeOutput(currentCaptureOutput)
+            self.position = self.position == .front ? .back : .front
+            self.configureSession()
+            self.captureSession.commitConfiguration()
+        }
+    }
+    
+    func stopSession() {
+        self.captureSession.stopRunning()
+    }
+    
+    func toggleFlash() {
+        var device : AVCaptureDevice!
+        
+        if #available(iOS 10.0, *) {
+            let videoDeviceDiscoverySession = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInWideAngleCamera, .builtInDuoCamera], mediaType: AVMediaTypeVideo, position: .unspecified)!
+            let devices = videoDeviceDiscoverySession.devices!
+            device = devices.first!
+            
+        } else {
+            // Fallback on earlier versions
+            device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        }
+        
+        if ((device as AnyObject).hasMediaType(AVMediaTypeVideo))
+        {
+            if (device.hasTorch)
+            {
+//                self.beginConfiguration()
+                //self.objOverlayView.disableCenterCameraBtn();
+                if device.isTorchActive == false {
+                    self.flashOn(device: device)
+                } else {
+                    self.flashOff(device: device);
+                }
+                //self.objOverlayView.enableCenterCameraBtn();
+//                self.session.commitConfiguration()
+            }
+        }
+    }
+    
+    private func flashOn(device:AVCaptureDevice)
+    {
+        do{
+            if (device.hasTorch)
+            {
+                try device.lockForConfiguration()
+                device.torchMode = .on
+                device.flashMode = .on
+                device.unlockForConfiguration()
+                self.flash = true
+            }
+        }catch{
+            //DISABEL FLASH BUTTON HERE IF ERROR
+            print("Device tourch Flash Error ");
+        }
+    }
+    
+    private func flashOff(device:AVCaptureDevice)
+    {
+        do{
+            if (device.hasTorch){
+                try device.lockForConfiguration()
+                device.torchMode = .off
+                device.flashMode = .off
+                device.unlockForConfiguration()
+                 self.flash = false
+            }
+        }catch{
+            //DISABEL FLASH BUTTON HERE IF ERROR
+            print("Device tourch Flash Error ");
+        }
+    }
+    
+    // MARK: AVSession configuration
+    private func checkPermission() {
+        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) {
+        case .authorized:
+            permissionGranted = true
+        case .notDetermined:
+            requestPermission()
+        default:
+            permissionGranted = false
+        }
+    }
+    
+    private func requestPermission() {
+        sessionQueue.suspend()
+        AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo) { [unowned self] granted in
+            self.permissionGranted = granted
+            self.sessionQueue.resume()
+        }
+    }
     
     private func configureSession() {
         guard permissionGranted else { return }
@@ -47,23 +144,21 @@ class AsynchronousCameraReading:  NSObject, AVCaptureVideoDataOutputSampleBuffer
         guard captureSession.canAddInput(captureDeviceInput) else { return }
         captureSession.addInput(captureDeviceInput)
         let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.setSampleBufferDelegate(self as AVCaptureVideoDataOutputSampleBufferDelegate, queue: DispatchQueue(label: "buffer images"))
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sample buffer"))
         guard captureSession.canAddOutput(videoOutput) else { return }
         captureSession.addOutput(videoOutput)
         guard let connection = videoOutput.connection(withMediaType: AVFoundation.AVMediaTypeVideo) else { return }
         guard connection.isVideoOrientationSupported else { return }
         guard connection.isVideoMirroringSupported else { return }
         connection.videoOrientation = .portrait
-     
+        connection.isVideoMirrored = position == .front
     }
     
     private func selectCaptureDevice() -> AVCaptureDevice? {
-        AVCaptureDeviceDiscoverySession(deviceTypes: [AVCaptureDeviceType.builtInDualCamera, AVCaptureDeviceType.builtInTelephotoCamera,AVCaptureDeviceType.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.back)
-        for device in (deviceDiscoverySession?.devices)! {
-            if(device.position == AVCaptureDevicePosition.back){
-                return device
-            }
-        }
+        return AVCaptureDevice.devices().filter {
+            ($0 as AnyObject).hasMediaType(AVMediaTypeVideo) &&
+                ($0 as AnyObject).position == position
+            }.first as? AVCaptureDevice
     }
     
     // MARK: Sample buffer to UIImage conversion
@@ -76,13 +171,9 @@ class AsynchronousCameraReading:  NSObject, AVCaptureVideoDataOutputSampleBuffer
     
     // MARK: AVCaptureVideoDataOutputSampleBufferDelegate
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
-        print("Got a frame!")
-        
-//        guard let uiImage = imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
-//        DispatchQueue.main.async { [unowned self] in
-//            self.delegate?.captured(image: uiImage)
-//        }
+        guard let uiImage = imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
+        DispatchQueue.main.async { [unowned self] in
+            self.delegate?.captured(image: uiImage)
+        }
     }
-
-
 }
--- CartTrackr/CartTrackr/CameraViewController.swift
+++ CartTrackr/CartTrackr/CameraViewController.swift
@@ -8,29 +8,30 @@
 
 import UIKit
 import AVFoundation
-import SwiftyCam
 import SwiftOCR
+import Lottie
 
-
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, FrameDelegate {
     
     var asynchronousCameraReading: AsynchronousCameraReading!
+    var previewLayer = AVCaptureVideoPreviewLayer();
     
     var flipCameraButton: UIButton!
     var flashButton: UIButton!
-    var captureButton: CameraButtonView!
-    var priceString: String? {
-        didSet {
-            OperationQueue.main.addOperation {
-//                self.performSegue(withIdentifier: ManualAddViewController.identifier, sender: nil)
-                self.dismiss(animated: true, completion: nil)
-                
-            }
-        }
-    }
+    var loadingAnimation: LOTAnimationView?
     
-    @IBOutlet weak var cameraViewTopBorder: UIView!
-    @IBOutlet weak var cameraViewBottomBorder: UIView!
+//    var priceString: String? {
+//        didSet {
+//            OperationQueue.main.addOperation {
+////                self.performSegue(withIdentifier: ManualAddViewController.identifier, sender: nil)
+//                self.dismiss(animated: true, completion: nil)
+//                
+//            }
+//        }
+//    }
+    
+    @IBOutlet var videoPreview: UIView!
+    @IBOutlet weak var imagePreview: UIImageView!
     
     //required init is needed when using AVFoundation
 //    required init?(coder aDecoder: NSCoder) {
@@ -40,17 +41,18 @@ class CameraViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        cameraDelegate = self
-//        shouldUseDeviceOrientation = true
         asynchronousCameraReading = AsynchronousCameraReading()
-        asynchronousCameraReading.delegate = self as? FrameDelegate
+        asynchronousCameraReading.delegate = self
         addButtons()
         
     }
-    
-    override func viewDidLayoutSubviews() {
 
+    func captured(image: UIImage) {
+        print("I wanna get this info\(image)")
+        imagePreview.frame = self.view.frame
+        imagePreview.image = image
     }
+    
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -72,78 +74,35 @@ class CameraViewController: UIViewController {
 //    }
     
     //Sends image to OCR, processes it and sets the priceString with the string that is returned
-    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didTake photo: UIImage) {
-        
-        OCRProcess.shared.process(targetImage: photo, callback: { (priceString) in
-            self.priceString = priceString
-            print("camera controller\(String(describing: self.priceString))")
-            
-        })
-        
-    }
-    
-    //Handles the logic for focusing the camera
-    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFocusAtPoint point: CGPoint) {
-        let focusView = UIImageView(image: #imageLiteral(resourceName: "focus"))
-        focusView.center = point
-        focusView.alpha = 0.0
-        view.addSubview(focusView)
-        
-        UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveEaseInOut, animations: {
-            focusView.alpha = 1.0
-            focusView.transform = CGAffineTransform(scaleX: 1.25, y: 1.25)
-        }, completion: { (success) in
-            UIView.animate(withDuration: 0.15, delay: 0.5, options: .curveEaseInOut, animations: {
-                focusView.alpha = 0.0
-                focusView.transform = CGAffineTransform(translationX: 0.6, y: 0.6)
-            }, completion: { (success) in
-                focusView.removeFromSuperview()
-            })
-        })
-    }
-    
-    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didChangeZoomLevel zoom: CGFloat) {
-        print(zoom)
-    }
-    
-    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didSwitchCameras camera: SwiftyCamViewController.CameraSelection) {
-        print(camera)
-    }
-    
-    
-    //Checker used to check authorization of using camera
-//    func checkCameraAuthorization(_ completionHandler: @escaping((_ authorized: Bool)-> Void)){
-//        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo){
+//    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didTake photo: UIImage) {
+//        
+//        OCRProcess.shared.process(targetImage: photo, callback: { (priceString) in
+//            self.priceString = priceString
+//            print("camera controller\(String(describing: self.priceString))")
 //            
-//        case .denied:
-//            completionHandler(false)
-//        case .authorized:
-//            completionHandler(true)
-//        case .notDetermined:
-//            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { success in
-//                completionHandler(success)
-//            })
-//        case .restricted:
-//            completionHandler(false)
-//        }
+//        })
+//        
 //    }
+
     
     //Calls function for swapping between front and rear cameras when pressed
-//    @objc private func cameraSwitchAction(_ sender: Any) {
-//        switchCamera()
-//    }
+    @objc private func cameraSwitchAction(_ sender: Any) {
+        asynchronousCameraReading.flipCamera()
+    }
     
-    //Toggles the flash on the camera
-//    @objc private func toggleFlashAction(_ sender: Any) {
-//        flashEnabled = !flashEnabled
-//        
-//        if flashEnabled == true {
-//            flashButton.setImage(#imageLiteral(resourceName: "flash"), for: UIControlState())
-//        } else {
-//            flashButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControlState())
-//        }
-//    }
-//    
+//    Toggles the flash on the camera
+    @objc private func toggleFlashAction(_ sender: Any) {
+        asynchronousCameraReading.toggleFlash()
+        
+        if (asynchronousCameraReading.flash) {
+            flashButton.setImage(#imageLiteral(resourceName: "flash"), for: UIControlState())
+        } else {
+            flashButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControlState())
+        }
+    }
+    private func scanningAnimation(){
+        
+    }
     //Creates the buttons
     private func addButtons() {
         drawBorder()
@@ -152,26 +111,25 @@ class CameraViewController: UIViewController {
         cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
         view.addSubview(cancelButton)
         
-        captureButton = CameraButtonView(frame: CGRect(x: view.frame.midX - 37.5, y: view.frame.height - 100.0, width: 75.0, height: 75.0))
+        loadingAnimation = CameraButtonView(frame: CGRect(x: view.frame.midX - 37.5, y: view.frame.height - 100.0, width: 75.0, height: 75.0))
         
-        self.view.addSubview(captureButton)
+        self.view.addSubview(loadingAnimation)
         
-//        captureButton.delegate = self
-        
-//        flipCameraButton = UIButton(frame: CGRect(x: (((view.frame.width / 2 - 37.5) / 2) - 15.0), y: view.frame.height - 74.0, width: 30.0, height: 23.0))
-//        flipCameraButton.setImage(#imageLiteral(resourceName: "flipCamera"), for: UIControlState())
-//        flipCameraButton.addTarget(self, action: #selector(cameraSwitchAction(_:)), for: .touchUpInside)
-//        self.view.addSubview(flipCameraButton)
+        flipCameraButton = UIButton(frame: CGRect(x: (((view.frame.width / 2 - 37.5) / 2) - 15.0), y: view.frame.height - 74.0, width: 30.0, height: 23.0))
+        flipCameraButton.setImage(#imageLiteral(resourceName: "flipCamera"), for: UIControlState())
+        flipCameraButton.addTarget(self, action: #selector(cameraSwitchAction(_:)), for: .touchUpInside)
+        self.view.addSubview(flipCameraButton)
         
         let test = CGFloat((view.frame.width - (view.frame.width / 2 + 37.5)) + ((view.frame.width / 2) - 37.5) - 9.0)
         
         flashButton = UIButton(frame: CGRect(x: test, y: view.frame.height - 77.5, width: 18.0, height: 30.0))
         flashButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControlState())
-//        flashButton.addTarget(self, action: #selector(toggleFlashAction(_:)), for: .touchUpInside)
+        flashButton.addTarget(self, action: #selector(toggleFlashAction(_:)), for: .touchUpInside)
         self.view.addSubview(flashButton)
     }
     
     func cancel() {
+        asynchronousCameraReading.stopSession()
         dismiss(animated: true, completion: nil)
     }
     
@@ -207,8 +165,3 @@ class CameraViewController: UIViewController {
     
 }
 
-extension CameraViewController: SwiftyCamViewControllerDelegate{
-    
-    
-    
-}
